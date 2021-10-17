b571deecd16553429fc32912647fe65b450e4c81
moazhamdy22 <moaz.hamdy1223@gmail.com>
2021-10-17 04:37:10 +0200

    adding 12 or 24

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-
+let twelve = true;
+const twelve_Button= document.querySelector('.twelve');
+const twentyfour_Button = document.querySelector('.twentyfour');
+twelve_Button.addEventListener('click',()=>{twelve=true;clock12();});
+twentyfour_Button.addEventListener('click',()=>{twelve=false;clock24();});
+clock12();
+function clock12(){
 function clock(){
+    document.getElementById('hour').innerHTML = "";
+    document.getElementById('minute').innerHTML ="";
+    document.getElementById('second').innerHTML ="";
+    function myStopFunction() {
+        clearInterval(idVar);
+    }
+    if (twelve ===false){
+        myStopFunction();
+    }
+    
     const fullDate = new Date();
-    var hours = fullDate.getHours();
-    var minutes = fullDate.getMinutes();
-    var seconds = fullDate.getSeconds();
+    let hours = fullDate.getHours();
+    let minutes = fullDate.getMinutes();
+    let seconds = fullDate.getSeconds();
 
     if(hours<10 && hours!==0){
         hours = '0'+hours;
@@ -35,4 +51,45 @@ function clock(){
     document.getElementById('minute').innerHTML = ":" + minutes;
     document.getElementById('second').innerHTML = ":" + seconds;
 }
-setInterval(clock, 1000);
+if(twelve === false){
+    clearInterval(idVar);
+}
+var idVar = setInterval(clock, 1000);
+
+}
+function clock24(){
+    function clock(){
+        document.getElementById('hour').innerHTML = "";
+        document.getElementById('minute').innerHTML ="";
+        document.getElementById('second').innerHTML ="";
+        function myStopFunction() {
+            clearInterval(idVar);
+        }
+        if(twelve ===true){
+            myStopFunction();
+        }
+        
+        const fullDate = new Date();
+        let hours = fullDate.getHours();
+        let minutes = fullDate.getMinutes();
+        let seconds = fullDate.getSeconds();
+
+        if(hours<10){
+            hours = '0'+hours;
+        }
+        if(minutes<10){
+            minutes = '0'+minutes;
+        }
+        if(seconds<10){
+            seconds = '0'+seconds;
+        }
+
+        document.getElementById('hour').innerHTML = hours;
+        document.getElementById('minute').innerHTML = ":" + minutes;
+        document.getElementById('second').innerHTML = ":" + seconds;
+
+    }
+    var idVar= setInterval(clock,1000);
+
+}
+console.log("hello");
